10dceff9d86b2e1a9e2b37e2ff7da0f69890ca30
Kizmet <bryantpatton@gmail.com>
2019-10-06 17:58:24 -0700

    scripts

--- src/models/index.js
+++ src/models/index.js
@@ -1,25 +1,25 @@
 import Sequelize from "sequelize";
 
-const sequelize = new Sequelize(
-  "postgres://bryant@localhost:5432/graphql_postgres"
-);
+// const sequelize = new Sequelize(
+//   "postgres://bryant@localhost:5432/graphql_postgres"
+// );
 
-// let sequelize;
-// if (process.env.DATABASE_URL) {
-//   sequelize = new Sequelize({
-//     connection: process.env.DATABASE_URL,
-//     dialect: "postgres"
-//   });
-// } else {
-//   sequelize = new Sequelize(
-//     process.env.TEST_DATABASE || process.env.DATABASE,
-//     process.env.DATABASE_USER,
-//     process.env.DATABASE_PASSWORD,
-//     {
-//       dialect: "postgres"
-//     }
-//   );
-// }
+let sequelize;
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres"
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.TEST_DATABASE || process.env.DATABASE,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    {
+      dialect: "postgres",
+      host: "/Users/bryant/Library/Application Support/Postgres/var-11"
+    }
+  );
+}
 
 const models = {
   User: sequelize.import("./user"),
